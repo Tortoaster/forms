48a05014d3a2759bd4c06fe7863980e6367e185b
Rick van der Wal <rickvanderwalvis@gmail.com>
2021-08-28 11:00:31 +0200

    Added example

---
+++ examples/rocket/main.rs
@@ -0,0 +1,36 @@
+#![cfg(feature = "rocket")]
+#[macro_use]
+extern crate rocket;
+
+use rocket::error::Error;
+
+use itasks::{Task, view};
+use itasks::component::Component;
+use itasks_derive::Component;
+
+#[derive(Component)]
+struct Field(String);
+
+#[derive(Component)]
+struct Struct {
+    field: Field,
+}
+
+#[get("/")]
+async fn index() -> Task<Struct> {
+    let component = Struct {
+        field: Field("Hello".to_owned())
+    };
+
+    view(component)
+}
+
+#[rocket::main]
+async fn main() -> Result<(), Box<Error>> {
+    rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
+}
