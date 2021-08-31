2dbc4958120e2ebe86236af94d99cb69caebe72e
Rick van der Wal <rickvanderwalvis@gmail.com>
2021-08-31 22:00:10 +0200

    Added or-combinator

--- src/task.rs
+++ src/task.rs
@@ -4,6 +4,7 @@ pub enum Task<C> {
     View(C),
     Enter,
     Update(C),
+    Either(Box<Task<C>>, Box<Task<C>>),
 }
 
 impl<C: Component> Task<C> {
@@ -11,8 +12,13 @@ impl<C: Component> Task<C> {
         match self {
             Task::View(component) => component.view(),
             Task::Enter => C::enter(),
-            Task::Update(mut component) => component.update()
+            Task::Update(mut component) => component.update(),
+            Task::Either(left, right) => format!("<div class=\"component\"><div class=\"content\">{}{}</div></div>", left.content(), right.content())
         }
+    }
+
+    pub fn or(self, other: Task<C>) -> Task<C> {
+        Task::Either(Box::new(self), Box::new(other))
     }
 }
 
