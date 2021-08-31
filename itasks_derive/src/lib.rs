6c335adb41254301802cb049a7d3519b6ad642eb
Rick van der Wal <rickvanderwalvis@gmail.com>
2021-08-31 21:21:08 +0200

    Implemented derive for enter and update tasks

--- itasks_derive/src/lib.rs
+++ itasks_derive/src/lib.rs
@@ -15,45 +15,127 @@ pub fn component_derive(input: TokenStream) -> TokenStream {
 
     match &ast.data {
         Data::Struct(data_struct) => {
-            let content = match &data_struct.fields {
+            match &data_struct.fields {
                 Fields::Named(fields_named) => {
-                    let names = fields_named.named.iter().map(|f| &f.ident);
-                    quote! {
-                    let content = vec![
-                        #(format!("{}: {}", stringify!(#names), self.#names.view())),*
-                    ];
-                }
+                    let views: TokenStream2 = fields_named.named.iter().flat_map(|field| {
+                        let name = &field.ident;
+                        quote_spanned! {field.span()=>
+                            format!("{}: {}", stringify!(#name), self.#name.view()),
+                        }
+                    }).collect();
+
+                    let enters: TokenStream2 = fields_named.named.iter().flat_map(|field| {
+                        let name = &field.ident;
+                        let ty = &field.ty;
+                        quote_spanned! {field.span()=>
+                            format!("{}: {}", stringify!(#name), #ty::enter()),
+                        }
+                    }).collect();
+
+                    let updates: TokenStream2 = fields_named.named.iter().flat_map(|field| {
+                        let name = &field.ident;
+                        quote_spanned! {field.span()=>
+                            format!("{}: {}", stringify!(#name), self.#name.update()),
+                        }
+                    }).collect();
+
+                    let gen = quote! {
+                        impl Component for #name {
+                            fn view(&self) -> String {
+                                let fields = vec![
+                                    #views
+                                ];
+                                format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), fields.join("<hr/>"))
+                            }
+
+                            fn enter() -> String {
+                                let fields = vec![
+                                    #enters
+                                ];
+                                format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), fields.join("<hr/>"))
+                            }
+
+                            fn update(&mut self) -> String {
+                                let fields = vec![
+                                    #updates
+                                ];
+                                format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), fields.join("<hr/>"))
+                            }
+                        }
+                    };
+
+                    gen.into()
                 }
                 Fields::Unnamed(fields_unnamed) => {
-                    let nums = (0..fields_unnamed.unnamed.len()).map(syn::Index::from);
-                    quote! {
-                    let content = vec![
-                        #(format!("{}", self.#nums.view())),*
-                    ];
-                }
+                    let views: TokenStream2 = (0..fields_unnamed.unnamed.len()).map(syn::Index::from).flat_map(|index| {
+                        quote! {
+                            format!("{}", self.#index.view())
+                        }
+                    }).collect();
+
+                    let enters: TokenStream2 = fields_unnamed.unnamed.iter().flat_map(|field| {
+                        let ty = &field.ty;
+                        quote! {
+                            format!("{}", #ty::enter())
+                        }
+                    }).collect();
+
+                    let updates: TokenStream2 = (0..fields_unnamed.unnamed.len()).map(syn::Index::from).flat_map(|index| {
+                        quote! {
+                            format!("{}", self.#index.update())
+                        }
+                    }).collect();
+
+                    let gen = quote! {
+                        impl Component for #name {
+                            fn view(&self) -> String {
+                                let fields = vec![
+                                    #views
+                                ];
+                                format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), fields.join("<hr/>"))
+                            }
+
+                            fn enter() -> String {
+                                let fields = vec![
+                                    #enters
+                                ];
+                                format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), fields.join("<hr/>"))
+                            }
+
+                            fn update(&mut self) -> String {
+                                let fields = vec![
+                                    #updates
+                                ];
+                                format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), fields.join("<hr/>"))
+                            }
+                        }
+                    };
+
+                    gen.into()
                 }
                 Fields::Unit => {
-                    quote! {
-                    let content = Vec::new();
-                }
-                }
-            };
+                    let gen = quote! {
+                        impl Component for #name {
+                            fn view(&self) -> String {
+                                format!("<div class=\"component\"><div class=\"title\">{}</div></div>", stringify!(#name))
+                            }
 
-            let gen = quote! {
-                impl Component for #name {
-                    fn view(&self) -> String {
-                        #content
-                        format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"content\">{}</div></div>", stringify!(#name), content.join("<hr/>"))
-                    }
-                }
-            };
+                            fn enter() -> String {
+                                format!("<div class=\"component\"><div class=\"title\">{}</div></div>", stringify!(#name))
+                            }
 
-            gen.into()
+                            fn update(&mut self) -> String {
+                                format!("<div class=\"component\"><div class=\"title\">{}</div></div>", stringify!(#name))
+                            }
+                        }
+                    };
+
+                    gen.into()
+                }
+            }
         }
         Data::Enum(data_enum) => {
-            let mut matches = TokenStream2::new();
-
-            for variant in &data_enum.variants {
+            let matches: TokenStream2 = data_enum.variants.iter().flat_map(|variant| {
                 let variant_name = &variant.ident;
 
                 let variant_fields = match &variant.fields {
@@ -62,19 +144,27 @@ pub fn component_derive(input: TokenStream) -> TokenStream {
                     Fields::Named(_) => quote_spanned! {variant.span()=> {..} },
                 };
 
-                matches.extend(quote_spanned! {variant.span()=>
+                quote_spanned! {variant.span()=>
                     #name::#variant_name #variant_fields => format!("<div class=\"component\"><div class=\"title\">{}</div><div class=\"subtitle\">{}</div><div class=\"content\"></div></div>", stringify!(#name), stringify!(#variant_name)),
-                });
-            }
+                }
+            }).collect();
 
             let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
 
             let gen = quote! {
-                impl #impl_generics #name #ty_generics #where_clause {
+                impl #impl_generics Component for #name #ty_generics #where_clause {
                     fn view(&self) -> String {
                         match self {
                             #matches
                         }
+                    }
+
+                    fn enter() -> String {
+
+                    }
+
+                    fn update(&mut self) -> String {
+
                     }
                 }
             };
